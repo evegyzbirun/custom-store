e450c2a5499971c989d63f0a096f005eb4059739
evegyzbirun <zbirunzbirun@gmail.com>
2023-02-27 18:53:26 -0800

    renamed StoreControl to ProductControl and more fix

---
+++ src/components/ProductControl.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import NewProductForm from './NewProductForm';
+import ProductList from './ProductList';
+import EditProductForm from './EditProductForm';
+import ProductDetail from './ProductDetail';
+
+class ProductControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainProductList: [],
+      selectedProduct: null,
+      editing: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedProduct != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedProduct: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+
+handleDeletingProduct = (id) => {
+  const newMainProductList = this.state.mainProductList.filter(product => product.id !== id);
+  this.setState({
+    mainProductList: newMainProductList,
+    selectedProduct: null
+  });
+}
+
+handleEditClick = () => {
+  this.setState({editing: true});
+}
+
+handleEditingProductInList = (productToEdit) => {
+  const editedMainProductList = this.state.mainProductList
+    .filter(product => product.id !== this.state.selectedProduct.id)
+    .concat(productToEdit);
+  this.setState({
+    mainProductList: editedMainProductList,
+    editing: false,
+    selectedProduct: null
+  });
+}
+
+handleAddingNewProductToList = (newProduct) => {
+  const newMainProductList = this.state.mainProductList.concat(newProduct);
+  this.setState({mainProductList: newMainProductList});
+  this.setState({formVisibleOnPage: false});
+}
+
+handleChangingSelectedProduct = (id) => {
+  const selectedProduct = this.state.mainProductList.filter(product => product.id === id)[0];
+  this.setState({selectedProduct: selectedProduct});
+}
+
+render(){
+  let currentlyVisibleState = null;
+  let buttonText = null; 
+  if (this.state.editing ) {      
+    currentlyVisibleState = <EditProductForm ticket = {this.state.selectedProduct} onEditProduct = {this.handleEditingProductInList} />
+    buttonText = "Return to Product List";
+  } else if (this.state.selectedProduct != null) {
+    currentlyVisibleState = <ProductDetail 
+    product={this.state.selectedProduct} 
+    onClickingDelete={this.handleDeletingProduct}
+    onClickingEdit = {this.handleEditClick} />
+    buttonText = "Return to Product List";
+  } else if (this.state.formVisibleOnPage) {
+    currentlyVisibleState = <NewProductForm onNewProductCreation={this.handleAddingNewProductToList}/>;
+    buttonText = "Return to Product List"; 
+  } else {
+    currentlyVisibleState = <ProductList onProductSelection={this.handleChangingSelectedProduct} productList={this.state.mainProductList} />;
+    buttonText = "Add Product"; 
+  }
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+      <button onClick={this.handleClick}>{buttonText}</button> 
+    </React.Fragment>
+  );
+  }
+}
+
+
+
+
+
+export default ProductControl;
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,3 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+function ProductDetail(props){
+  const { product, onClickingDelete, onClickingEdit } = props; 
+
+  return (
+    <React.Fragment>
+      <h1>product Detail</h1>
+      <h3>{product.sku} - {product.names}</h3>
+      <p><em>{product.color}</em></p>
+      <p><em>{product.category}</em></p>
+      <p><em>{product.picture}</em></p>
+      <button onClick={onClickingEdit}>Update Product</button>
+      <button onClick={()=> onClickingDelete(product.id)}>Close Product</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+ProductDetail.propTypes = {
+  product: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func 
+};
+
+export default ProductDetail;

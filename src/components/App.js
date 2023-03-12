eaccf0bbe3e28341bfeaab62dcc357cfb37b893d
evegyzbirun <zbirunzbirun@gmail.com>
2023-03-11 19:51:02 -0800

    working on cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Basket from "./Basket";
 import Header from "./Header";
-// import Main from "./Main";
+import PropTypes from "prop-types";
 import ProductControl from "./ProductControl";
+import ProductList from './ProductList';
 
 
 
 function App() {
+  const { products } = ProductList;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exit = cartItems.find(x => x.id === product.id);
+    if(exit) {
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exit, qty: exit.qty +1 } :x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, {...products, qty: 1}])
+    }
+  }
 
 
   return (
-
-
     <div className="App">
       <Header></Header>
       <div className="row">
         {/* <Main></Main> */}
         {/* <ProductControl></ProductControl> */}
-        <Basket></Basket>
+        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
       </div>
-      <ProductControl></ProductControl>
+      <ProductControl onAdd={onAdd} products={products}></ProductControl>
     </div>
 
 
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,10 +2,15 @@ import React from "react";
 
 
 function Basket(props) {
+
+  const {cartItems} = props
   return (
 
     <aside className="block col-1">
       <h2>Cart Items</h2>
+      <div>
+        {cartItems.length === 0 && <div>Cart Is Empty</div>}
+      </div>
     </aside>
 
   );
